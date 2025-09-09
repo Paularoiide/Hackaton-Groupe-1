29248e7bb5e55d457225fb107fcb787c1b8c6e75
Paul Andreis <paul.andreis@eleves.enpc.fr>
2025-09-09 12:47:53 +0200

    Descente AIC/BIC

--- TEST.py
+++ TEST.py
@@ -23,9 +23,9 @@ dataset.info()
 dataset.head()
 
 #Remplir les missing values avec infini dans 'TIME_TO_PARADE_1','TIME_TO_PARADE_2','TIME_TO_NIGHT_SHOW'
-dataset['TIME_TO_PARADE_1'].fillna(np.inf, inplace=True)
-dataset['TIME_TO_PARADE_2'].fillna(np.inf, inplace=True)
-dataset['TIME_TO_NIGHT_SHOW'].fillna(np.inf, inplace=True)
+dataset['TIME_TO_PARADE_1'].fillna(1000000, inplace=True)
+dataset['TIME_TO_PARADE_2'].fillna(1000000, inplace=True)
+dataset['TIME_TO_NIGHT_SHOW'].fillna(1000000, inplace=True)
 
 dataset.head()
 
@@ -42,3 +42,44 @@ dataset.head()
 predictors = ['DAY_OF_WEEK', 'DAY', 'HOUR','ADJUST_CAPACITY','DOWNTIME','CURRENT_WAIT_TIME','TIME_TO_PARADE_1','TIME_TO_PARADE_2','TIME_TO_NIGHT_SHOW']
 X = dataset[predictors]
 y = dataset['WAIT_TIME_IN_2H'] # Response variable
+
+# AIC and BIC based stepwise forward selection
+
+# Define the initial selected and unselected predictors
+selected_predictors = ['const'] # Start with only the intercept
+unselected_predictors = ['ADJUST_CAPACITY','DOWNTIME','CURRENT_WAIT_TIME','TIME_TO_PARADE_1','TIME_TO_PARADE_2','TIME_TO_NIGHT_SHOW']
+X = sm.add_constant(X)  # Ajoute une constante si elle n'existe pas déjà
+
+  
+# Compute the Information Criterion (IC) for the model with only the intercept
+current_ic = sm.OLS(y, X[selected_predictors]).fit().aic # AIC
+
+ic_values = [current_ic]  # Store successive IC values
+
+# Stepwise forward selection based on minimizing IC
+while len(unselected_predictors)>0:
+    best_ic = np.inf  # Initialize with a very high IC
+    best_predictor = None
+
+    # Try adding each unselected predictor one by one
+    for pred in unselected_predictors:
+        test_model = sm.OLS(y, X[selected_predictors + [pred]]).fit()
+        test_ic = test_model.aic # AIC
+
+        # Check if this predictor gives the lowest IC so far
+        if test_ic < best_ic:
+            best_ic = test_ic
+            best_predictor = pred
+
+    # If the best IC is lower than the current IC, accept the predictor
+    if best_ic < current_ic:
+        selected_predictors.append(best_predictor)
+        unselected_predictors.remove(best_predictor)
+        ic_values.append(best_ic)
+        current_ic = best_ic  # Update current IC
+    else:
+        break  # Stop if no improvement
+
+# Print results
+print("Selected predictors:", selected_predictors)
+print("IC values over iterations:", ic_values)
