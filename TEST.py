7870aacb92e968e494fde4f0caf588c7b6d338cf
Paul Andreis <paul.andreis@eleves.enpc.fr>
2025-09-09 11:07:26 +0200

    Importation des librairies et du dataset

--- TEST.py
+++ TEST.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 from sklearn.model_selection import train_test_split
@@ -7,6 +9,15 @@ from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.decomposition import PCA
 
+print("Numpy:", np.__version__)
+print("Matplotlib:", plt.matplotlib.__version__)
+print("Pandas:", pd.__version__)
+print("Statsmodels:", sm.__version__)
+
 def RMSE(x,y):
   return np.sqrt(mean_squared_error(x,y))
 
+# Put the dataset into a pandas DataFrame
+dataset = pd.read_table('waiting_times_train.csv', sep=',', decimal='.')
+dataset.info()
+dataset.head()
