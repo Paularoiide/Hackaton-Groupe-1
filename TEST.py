d1a934dd0308c71cdde9d164ed8946c3ac03ea1b
clara <clara.lima-goldenberg@enpc.fr>
2025-09-09 13:34:25 +0200

    fonction

--- TEST.py
+++ TEST.py
@@ -22,24 +22,25 @@ dataset = pd.read_table('waiting_times_train.csv', sep=',', decimal='.')
 dataset.info()
 dataset.head()
 
-#Remplir les missing values avec infini dans 'TIME_TO_PARADE_1','TIME_TO_PARADE_2','TIME_TO_NIGHT_SHOW'
-dataset['TIME_TO_PARADE_1'] = dataset['TIME_TO_PARADE_1'].fillna(10000)
-dataset['TIME_TO_PARADE_2'] = dataset['TIME_TO_PARADE_2'].fillna(10000)
-dataset['TIME_TO_NIGHT_SHOW'] = dataset['TIME_TO_NIGHT_SHOW'].fillna(10000)
 
-#On crée une nouvelle colonne 'TIME_TO_PARADE_UNDER_2H' qui vaut 1 si un des deux parades a lieu dans les 2h, 0 sinon
-dataset['TIME_TO_PARADE_UNDER_2H'] = np.where((abs(dataset['TIME_TO_PARADE_1']) <= 120) | (abs(dataset['TIME_TO_PARADE_2']) <= 120), 1, 0)
+def adapter_dataset(dataset):
+    #Remplir les missing values avec infini dans 'TIME_TO_PARADE_1','TIME_TO_PARADE_2','TIME_TO_NIGHT_SHOW'
+    dataset['TIME_TO_PARADE_1'] = dataset['TIME_TO_PARADE_1'].fillna(10000)
+    dataset['TIME_TO_PARADE_2'] = dataset['TIME_TO_PARADE_2'].fillna(10000)
+    dataset['TIME_TO_NIGHT_SHOW'] = dataset['TIME_TO_NIGHT_SHOW'].fillna(10000)
 
-#On cherche à rendre utilisable DATETIME : on va le separer en trois paramètres --> jour de la semaine, date (dans le type date) et heure de la journée (avec heure et minute)
-dataset['DATETIME'] = pd.to_datetime(dataset['DATETIME'])
-dataset['DAY_OF_WEEK'] = dataset['DATETIME'].dt.dayofweek
-dataset['DAY'] = dataset['DATETIME'].dt.day
-dataset['MONTH'] = dataset['DATETIME'].dt.month
-dataset['YEAR'] = dataset['DATETIME'].dt.year
-dataset['HOUR'] = dataset['DATETIME'].dt.hour
-dataset['MINUTE'] = dataset['DATETIME'].dt.minute
-dataset.head()
+    #On cherche à rendre utilisable DATETIME : on va le separer en trois paramètres --> jour de la semaine, date (dans le type date) et heure de la journée (avec heure et minute)
+    dataset['DATETIME'] = pd.to_datetime(dataset['DATETIME'])
+    dataset['DAY_OF_WEEK'] = dataset['DATETIME'].dt.dayofweek
+    dataset['DAY'] = dataset['DATETIME'].dt.day
+    dataset['MONTH'] = dataset['DATETIME'].dt.month
+    dataset['YEAR'] = dataset['DATETIME'].dt.year
+    dataset['HOUR'] = dataset['DATETIME'].dt.hour
+    dataset['MINUTE'] = dataset['DATETIME'].dt.minute
+    dataset['TIME_TO_PARADE_UNDER_2H'] = np.where((abs(dataset['TIME_TO_PARADE_1']) <= 120) | (abs(dataset['TIME_TO_PARADE_2']) <= 120), 1, 0)
 
+adapter_dataset(dataset)
+    
 predictors = ['DAY_OF_WEEK', 'DAY', 'MONTH', 'YEAR', 'HOUR', 'MINUTE', 'ADJUST_CAPACITY','DOWNTIME','CURRENT_WAIT_TIME','TIME_TO_PARADE_1','TIME_TO_PARADE_2','TIME_TO_NIGHT_SHOW', 'TIME_TO_PARADE_UNDER_2H']
 X = dataset[predictors]
 y = dataset['WAIT_TIME_IN_2H'] # Response variable
